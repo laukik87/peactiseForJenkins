7a617558068b8b7bf27a38b44100b7bcc84f410f
laukik87 <67040100+laukik87@users.noreply.github.com>
2024-03-24 13:03:21 +0530

    Update Hello_file.java

--- Hello_file.java
+++ Hello_file.java
@@ -1,7 +1,7 @@
-public class App 
+public class Hello_file 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World from git!" );
     }
 }
